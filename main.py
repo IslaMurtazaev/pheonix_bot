50a2897ae1db493f76e64d45c854ab6c301b14bd
Islam Murtazaev <islam.muratazaev@gmail.com>
2022-11-14 22:39:44 +0600

    Add command handlers

--- main.py
+++ main.py
@@ -9,8 +9,15 @@ TELEGRAM_KEY = os.getenv('TELEGRAM_KEY')
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    await update.message.reply_text(f'I hope you know what you are getting into {update.effective_user.first_name}')
 
+
+async def track(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Good luck')
+
+
+async def complete(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Woohoo good job!')
 
 app = ApplicationBuilder().token(TELEGRAM_KEY).build()
 
