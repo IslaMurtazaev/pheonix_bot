32176c497c407c6667add0057ac4efd9b977521a
ulanbekovs <aktan.ulanbekov@gmail.com>
2022-11-27 22:05:59 +0600

    add buttons in complete command

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler
 
 from database_utils import create_user, creat_tasks, setup_database, get_today_tasks
 
@@ -29,13 +29,28 @@ async def track(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 async def complete(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    get_today_tasks()
-    await update.message.reply_text(f'Woohoo good job!')
+    tasks = get_today_tasks()
+    keyboard = []
+    for task in tasks:
+        keyboard.append([InlineKeyboardButton(task[3], callback_data="Хорошая работа")])
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text(f'Woohoo good job!', reply_markup=reply_markup)
+
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
+
+    await query.answer()
+
+    await query.edit_message_text(text=f" {query.data}")
+
 
 app = ApplicationBuilder().token(TELEGRAM_KEY).build()
-
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("track", track))
 app.add_handler(CommandHandler("complete", complete))
+app.add_handler(CallbackQueryHandler(button))
 
 app.run_polling()
